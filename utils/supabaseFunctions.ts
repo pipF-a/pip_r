4d9574d6b97133485ad5d18be8afbb834e9d791a
pipF-a <pip87213@gmail.com>
2025-04-17 16:07:50 +0900

    todo削除関数を追加

--- utils/supabaseFunctions.ts
+++ utils/supabaseFunctions.ts
@@ -8,20 +8,31 @@ interface Todo {
   title: string;
 }
 
-
+// 取得関数
 export const getAllTodos = async (): Promise<Todo[]> => {
-  const { data, error } = await supabase.from("todo").select("*");
+  const { data, error } = await supabase.from("todos").select("*");
   if (error) {
     throw new Error(error.message);
   }
   return data as Todo[];
 }
 
+// 追加関数
 export const addTodos = async (formData:FormData) => {
   const todoTitle = formData.get('todotext') as string;
     if (!todoTitle?.trim()){
       return;
     };
-  await supabase.from('todo').insert({title:todoTitle})
+  await supabase.from('todos').insert({title:todoTitle})
   revalidatePath('/');
+}
+
+// 削除関数
+export const deleteTodo = async (formData:FormData) => {
+  const id = formData.get('id') as string;
+  await supabase
+  .from('todos')
+  .delete()
+  .eq('id', id)
+  revalidatePath('/')
 }
